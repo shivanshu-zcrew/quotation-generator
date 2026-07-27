a2ad4f30653ba37d49e364179349b9b7cf05ca15
shivanshu-zcrew <shivanshu.tyagi@zcrewindia.com>
2026-07-27 18:44:50 +0530

    added third company

--- frontend/quotation/src/page/AdminDashboard.jsx
+++ frontend/quotation/src/page/AdminDashboard.jsx
@@ -744,7 +744,7 @@ export default function AdminDashboard({ onNavigate, onViewQuotation }) {
           <button onClick={() => onNavigate?.('addQuotation')} style={headerBtn('green')}><FileText size={isMobile ? 12 : 14} /> {isMobile ? 'New' : 'New Quotation'}</button>
           <button onClick={() => onNavigate?.('userStats')} style={headerBtn('soft')}><TrendingUp size={isMobile ? 12 : 14} /> {!isMobile && 'User Stats'}</button>
           <button onClick={() => onNavigate?.('users')} style={headerBtn('soft')}><Users size={isMobile ? 12 : 14} /> {!isMobile && 'Users'}</button>
-          {/* <button onClick={() => onNavigate?.('companies')} style={headerBtn('soft')}><Building2 size={isMobile ? 12 : 14} /> {!isMobile && 'Companies'}</button> */}
+          <button onClick={() => onNavigate?.('companies')} style={headerBtn('soft')}><Building2 size={isMobile ? 12 : 14} /> {!isMobile && 'Companies'}</button>
           <button onClick={handleLogout} style={headerBtn('ghost')}><LogOut size={isMobile ? 12 : 15} /> {!isMobile && 'Logout'}</button>
         </div>
       </div>
